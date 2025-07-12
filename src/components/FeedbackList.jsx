ff3cbba2da924d70356c96fb856b264772d93c8c
Somayyeh094 <s.somayyeh.seyedi@gmail.com>
2025-07-12 19:21:09 +0330

    added framer motion into feedbackList

--- src/components/FeedbackList.jsx
+++ src/components/FeedbackList.jsx
@@ -1,22 +1,37 @@
 import React from "react";
+import { AnimatePresence, motion } from "framer-motion";
 import FeedbackItem from "./FeedbackItem.jsx";
 import Card from "./shared/Card";
 function FeedbackList(props) {
-    if (!props.feedbacks || props.feedbacks.length === 0) {
-        return (
-          <Card>
-            <p>No feedback yet!</p>
-          </Card>
-        );
-    } else {
-        return (
-            <div className="FeedbackList">
+  if (!props.feedbacks || props.feedbacks.length === 0) {
+    return (
+      <Card>
+        <p>No feedback yet!</p>
+      </Card>
+    );
+  } else {
+    return (
+      <div className="FeedbackList">
+        <AnimatePresence>
                 {props.feedbacks.map((feedback, index) => {
-                    return <FeedbackItem key={index} item={feedback} handleDelete={props.handleDelete} />;
-                })}
-            </div>
-        );
-    }
+              return(
+            <motion.div
+              key={index}
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              exit={{ opacity: 0}}
+            >
+              <FeedbackItem
+                key={index}
+                item={feedback}
+                handleDelete={props.handleDelete}
+              />
+            </motion.div>)
+          })}
+        </AnimatePresence>
+      </div>
+    );
+  }
 }
 
 export default FeedbackList;
